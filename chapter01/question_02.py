829309e2845412a1d894d33c85a51937516ee778
bulbulpaul <bulbulpaul0325@gmail.com>
2015-03-31 12:02:17 +0900

    Optimization q2

--- chapter01/question_02.py
+++ chapter01/question_02.py
@@ -1,10 +1,13 @@
 # coding: utf-8
 
 def alternately_merge(x ,y):
-    return ''.join([item for sublist in zip(x,y) for item in sublist])
+    return ''.join([sub_x + sub_y for sub_x, sub_y in zip(x,y)])
+
+def easy_alternately_merge(x ,y):
+    return ''.join(sum(zip(x, y), ()))
 
 def test_alternately_merge():
     assert alternately_merge('パトカー', 'タクシー') == 'パタトクカシーー'
     
 if __name__ == '__main__':
-    print(alternately_merge('パトカー', 'タクシー'))
+    print(easy_alternately_merge('パトカー', 'タクシー'))
