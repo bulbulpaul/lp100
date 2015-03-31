85e6a66ab6e1bf1d3d79ec1bc13c2dff515cc8ac
bulbulpaul <bulbulpaul0325@gmail.com>
2015-03-31 12:07:04 +0900

    Added decryption test, Modified char number

--- chapter01/question_08.py
+++ chapter01/question_08.py
@@ -2,13 +2,17 @@
 
 
 def cipher(x):
-    chars = [chr(216-ord(char)) if char.islower() else char for char in list(x)]
-    print(str(chars))
+    chars = [chr(219-ord(char)) if char.islower() else char for char in list(x)]
     return ''.join(chars)
 
 def test_template_string():
     word = 'test!テスト&Test'
-    assert cipher(word) == 'dsed!テスト&Tsed'
+    ciphe_text = cipher(word)
+
+    # encryption
+    assert ciphe_text == 'gvhg!テスト&Tvhg'
+    # decryption
+    assert cipher(ciphe_text) == word
 
 if __name__ == '__main__':
     word = 'test!テスト&Test'
