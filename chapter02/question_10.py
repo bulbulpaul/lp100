7eefbd32152bdc3420500ee4eeea4a56324d3584
bulbulpaul <bulbulpaul0325@gmail.com>
2015-06-11 23:04:57 +0900

    Delete unnecessary processing

--- chapter02/question_10.py
+++ chapter02/question_10.py
@@ -4,19 +4,20 @@ import argparse
 
 def init_parser():
     parser = argparse.ArgumentParser(description='Word count')
-    parser.add_argument('-l', '--lines', action = 'store_true', dest = 'lines', help='return the number of rows')
     parser.add_argument('file', help='target file')
     return parser
 
 def line_count(file_name):
-    return sum(1 for l in open(file_name))
+    result = 0
+    with open(file_name) as f:
+        result = sum(1 for l in f)
+    return result
 
 def test_line_count():
     # argument setting 
     parser = init_parser()
-    args = parser.parse_args(['-l', '../resource/hightemp.txt'])
-    assert(True, args.lines)
-    assert(24, line_count(args.file))
+    args = parser.parse_args(['../resource/hightemp.txt'])
+    assert 24 == line_count(args.file)
 
 if __name__ == '__main__':
 
@@ -24,7 +25,7 @@ if __name__ == '__main__':
     parser = init_parser()
     # parse
     args = parser.parse_args()
-    if args.lines:
+    if args.file:
         # line count
         print(line_count(args.file))
     else :
